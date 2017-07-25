5ced50ecadd89d6149062ee8fc2e4a5f314dbc49
Filipe Ceccon de Alencar <filipececcon@gmail.com>
2017-07-25 12:23:36 -0300

    ajuste

--- install.bat
+++ install.bat
@@ -8,17 +8,16 @@ ECHO INSTALLING SERVICE %SERVICENAME%
 %NSSM% stop %SERVICENAME%
 %NSSM% remove %SERVICENAME% confirm
 %NSSM% install %SERVICENAME% %SERVICENAME%
-
 %NSSM% set %SERVICENAME% Application %CD%\initializer.bat
 %NSSM% set %SERVICENAME% AppDirectory %CD%
 %NSSM% set %SERVICENAME% Description "Node Windows Service test"
 %NSSM% set %SERVICENAME% Start SERVICE_AUTO_START
-%NSSM% set %SERVICENAME% AppStopMethodSkip 1
+%NSSM% set %SERVICENAME% AppStopMethodSkip 0
 %NSSM% set %SERVICENAME% AppStopMethodConsole 0
 %NSSM% set %SERVICENAME% AppStopMethodWindow 0
 %NSSM% set %SERVICENAME% AppStopMethodThreads 0
 %NSSM% set %SERVICENAME% AppThrottle 0
-%NSSM% set %SERVICENAME% AppExit Default Restart
+%NSSM% set %SERVICENAME% AppExit Default Ignore
 %NSSM% set %SERVICENAME% AppRestartDelay 0
 %NSSM% set %SERVICENAME% AppStdout %CD%\logs\%SERVICENAME%.log
 %NSSM% set %SERVICENAME% AppStderr %CD%\logs\%SERVICENAME%.log
@@ -28,6 +27,3 @@ ECHO INSTALLING SERVICE %SERVICENAME%
 %NSSM% set %SERVICENAME% AppRotateOnline 0
 %NSSM% set %SERVICENAME% AppRotateSeconds 3600
 %NSSM% set %SERVICENAME% AppRotateBytes 524288
-%NSSM% set %SERVICENAME% AppPriority NORMAL_PRIORITY_CLASS
-%NSSM% set %SERVICENAME% AppNoConsole 0
-%NSSM% set %SERVICENAME% AppAffinity All
